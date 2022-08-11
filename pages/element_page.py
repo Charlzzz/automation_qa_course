cc216baa6f503145df7ce35c24376343eb026536
Charlzzz <nts.sergei@yandex.ru>
2022-08-12 00:14:02 +0700

    test radiobutton

--- pages/element_page.py
+++ pages/element_page.py
@@ -2,7 +2,7 @@ import random
 import time
 
 from generator.generator import generated_person
-from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators
 from pages.base_page import BasePage
 
 
@@ -64,5 +64,20 @@ class CheckBoxPage(BasePage):
         for item in result_list:
             data.append(item.text)
         return str(data).replace(" ", "").lower()
+
+class RadioButtonPage(BasePage):
+    locators = RadioButtonPageLocators()
+
+    def click_on_radio_button(self, choice):
+        choices = {
+        "yes": self.locators.YES_RADIOBUTTON,
+        "impressive": self.locators.IMPRESSIVE_RADIOBUTTON,
+        "no": self.locators.NO_RADIOBUTTON
+        }
+        radio = self.elements_are_visible(choices[choice]).click()
+
+    def get_output_result(self):
+        return self.element_is_present(self.locators.OUTPUT_RESULT).text
+
 
 
