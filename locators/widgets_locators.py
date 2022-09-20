eeef33ada45089779e814c3096dfd35e8aa84623
Charlzzz <nts.sergei@yandex.ru>
2022-09-20 22:54:12 +0700

    negative massage TestAutoCompletePage generated_color, dataclass Colors

--- locators/widgets_locators.py
+++ locators/widgets_locators.py
@@ -11,7 +11,12 @@ class AccordianPageLocators:
     SECTION_CONTENT_THIRD = (By.CSS_SELECTOR, "div[id='section3Content'] p")
 
 
-
+class AutoCompletePageLocators:
+    MULTI_INPUT = (By.CSS_SELECTOR, "input[id='autoCompleteMultipleInput']")
+    MULTI_VALUE = (By.CSS_SELECTOR, "div[class='css-1rhbuit-multiValue auto-complete__multi-value']")
+    MULTI_VALUE_REMOVE = (By.CSS_SELECTOR, "div[class='css-1rhbuit-multiValue auto-complete__multi-value'] svg path")
+    SINGLE_VALUE = (By.CSS_SELECTOR, "div[class='auto-complete__single-value css-1uccc91-singleValue']")
+    SINGLE_INPUT = (By.CSS_SELECTOR, "input[id='autoCompleteSingleInput']")
 
 
 
