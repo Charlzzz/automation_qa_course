fb3e60ef0657890038bce18944f4ac24574362ea
Charlzzz <nts.sergei@yandex.ru>
2022-09-21 23:24:15 +0700

    test_picker_date test_change_date_and_time

--- generator/generator.py
+++ generator/generator.py
@@ -1,9 +1,10 @@
 import random
 
-from data.data import Person, Color
+from data.data import Person, Color, Date
 from faker import Faker
 
 faker_ru = Faker("ru_RU")
+faker_en = Faker("En")
 Faker.seed()
 
 
@@ -34,3 +35,10 @@ def generated_color():
         color_name=["Red", "Blue", "Green", "Yellow", "Purple", "Black", "White", "Voilet", "Indigo", "Magenta", "Aqua"]
     )
 
+def generated_date():
+    yield Date(
+        year=faker_en.year(),
+        month=faker_en.month_name(),
+        day=faker_en.day_of_month(),
+        time="12:00"
+    )
